d0b0a63a3be9c75ea31dd83fe0ccbed42f20d9e0
Amanda Thorne <manda.thorne@gmail.com>
2019-03-01 20:09:25 -0600

    hero formatting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
 import Hero from './components/Hero.js';
+import Spotlight from './components/Spotlight.js';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Hero />
+        <Spotlight />
       </div>
     );
   }
---
+++ src/components/Spotlight.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Spotlight extends Component {
+  render() {
+    return (
+      <section className="spotlight-container">
+        <img src="http://placekitten.com/500/300"/>
+      </section>
+    );
+  }
+}
+
+export default Spotlight;
