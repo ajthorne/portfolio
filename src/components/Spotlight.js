7d3147b060d6674c391a4d63f8c68db9a396c700
Amanda Thorne <manda.thorne@gmail.com>
2019-03-06 19:35:20 -0600

    hero styles

--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -5,7 +5,11 @@ class Spotlight extends Component {
   render() {
     return (
       <section className="spotlight-container">
-        <img className="featured-img" src="/assets/booknook1.png"/>
+        <img className="featured-img" src="/assets/codingbg.jpeg"/>
+        <div className="featured-text">
+          <p>Clever joke goes here... ?</p>
+          <button>View Portfolio</button>
+        </div>
       </section>
     );
   }
